5f52be0a9474b91d5138885b366e6f8e29a917ca
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-09-07 22:06:20 +0200

    Added zip gem

--- Mono2.x/bootstrap.sh
+++ Mono2.x/bootstrap.sh
@@ -6,6 +6,7 @@ apt-get install -y git
 apt-get install -y mono-complete
 apt-get install -y ruby1.9.3
 gem install --no-rdoc --no-ri rake
+gem install --no-rdoc --no-ri zip
 gem install --no-rdoc --no-ri albacore
 
 cd /vagrant
--- Mono3.x/bootstrap.sh
+++ Mono3.x/bootstrap.sh
@@ -9,6 +9,7 @@ apt-get update
 apt-get install -y mono-complete
 apt-get install -y ruby1.9.3
 gem install --no-rdoc --no-ri rake
+gem install --no-rdoc --no-ri zip
 gem install --no-rdoc --no-ri albacore
 
 cd /vagrant
