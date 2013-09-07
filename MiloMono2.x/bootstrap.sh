3c00e4b728f768b5b6a10ae415f003c16847c09d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-09-07 21:58:15 +0200

    first commit

---
+++ MiloMono2.x/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get upgrade
+apt-get install -y git
+apt-get install -y mono-complete
+apt-get install -y ruby1.9.3
+gem install --no-rdoc --no-ri rake
+gem install --no-rdoc --no-ri albacore
+
+cd /vagrant
+echo cd \/vagrant > /home/vagrant/.bashrc
+rm -rf /etc/motd
+
+echo Milo on Mono >> /etc/motd
+echo ------------- >> /etc/motd
+echo >> /etc/motd
---
+++ MiloMono3.x/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+apt-get upgrade
+apt-get update
+apt-get install -y git
+apt-get install -y python-software-properties
+add-apt-repository ppa:directhex/monoxide
+apt-get update
+apt-get install -y mono-complete
+apt-get install -y ruby1.9.3
+gem install --no-rdoc --no-ri rake
+gem install --no-rdoc --no-ri albacore
+
+cd /vagrant
+echo cd \/vagrant > /home/vagrant/.bashrc
+rm -rf /etc/motd
+
+echo Milo on Mono >> /etc/motd
+echo ------------- >> /etc/motd
+echo >> /etc/motd
