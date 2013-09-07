300eb60a45ef50e15b04459c8e6246c370e3dbee
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-09-07 23:01:40 +0200

    Fixed so monodoc-browser won't complain

--- Mono3.x/bootstrap.sh
+++ Mono3.x/bootstrap.sh
@@ -7,6 +7,8 @@ apt-get install -y python-software-properties
 add-apt-repository ppa:directhex/monoxide
 apt-get update
 apt-get install -y mono-complete
+rm -r /var/lib/dpkg/info/monodoc-browser.*
+apt-get install -f
 apt-get install -y ruby1.9.3
 gem install --no-rdoc --no-ri rake
 gem install --no-rdoc --no-ri zip
